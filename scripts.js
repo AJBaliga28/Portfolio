4e7300bd84d2f0ab6980a4e3eb4b269c7c076336
AJBaliga28 <ajbaliga28@pop-os.localdomain>
2024-06-19 20:02:11 +0530

    Initial Commit.

---
+++ scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
